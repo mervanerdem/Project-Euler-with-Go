19ef3da13d55c1a81be53abc802ac0beee5f5101
Mervan <38705565+mervanerdem@users.noreply.github.com>
2022-03-06 19:57:18 +0300

    Promblem Solved.

---
+++ 05-Smallest multiple/main.go
@@ -0,0 +1,34 @@
+/*
+Smallest multiple
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+package main
+
+import "fmt"
+
+func main() {
+	a := 2520 * 11 * 12 * 13 * 14 * 15 * 16 * 17 * 18 * 19 * 20
+	b := 0
+	result := 0
+	for i := 2520; i < a; i++ {
+		for j := 1; j <= 20; j++ {
+			if i%j == 0 {
+				b++
+				if b == 20 {
+					result = i
+					break
+				}
+			} else {
+				b = 0
+				break
+			}
+		}
+		if b == 20 {
+			result = i
+			break
+		}
+	}
+	fmt.Println(result)
+
+}
